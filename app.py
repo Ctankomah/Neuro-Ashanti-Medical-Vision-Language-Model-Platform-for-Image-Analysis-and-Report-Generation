4f979e09d71d97c32affdc19a5f4d6c854489673
Livingstone Eli Ayivor <103898284+Leayivor@users.noreply.github.com>
2025-03-19 21:52:41 +0000

    modified:   app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from processing import Preprocessor
+
+preprocessor = Preprocessor(target_size=(128, 128))
+
+# Streamlit app
+st.title("Image Preprocessing")
+st.write("Upload an image to preprocess it.")
+
+# File uploader
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png", "bmp"])
+
+if uploaded_file is not None:
+    # Preprocess the image
+    try:
+        preprocessed_image = preprocessor.preprocess_file(uploaded_file)
+        st.write("Preprocessed image shape:", preprocessed_image.shape)
+
+        # Display the preprocessed image
+        st.image(preprocessed_image, caption="Preprocessed Image", use_column_width=True)
+    except Exception as e:
+        st.error(f"Error processing the image: {e}")
